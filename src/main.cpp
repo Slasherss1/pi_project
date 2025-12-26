7fffd80a662ae46fa695d54ac0540e24deca1f08
Jan Szumotalski <jszumotalski@gmail.com>
2025-12-26 12:24:44 +0100

    Added raylib

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <raylib.h>
 
 using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
+    InitWindow(800, 600, "Hello Raylib");
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello, Raylib!", 190, 200,20, LIGHTGRAY);
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
