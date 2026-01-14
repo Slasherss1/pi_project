431b583fd8dd43fef8d2b3b2fc58509d9db539b9
aszynali <aszynali@akamai.com>
2026-01-14 17:26:14 +0100

    Ustawienia

--- src/game.cpp
+++ src/game.cpp
@@ -1,14 +1,29 @@
 #include "game.h"
 #include "utils.h"
 
+static const char* GAME_NAME = "Flanki";
+static const char* NEW_GAME = "Nowa gra";
+static const char* LOAD_GAME = "Wczytaj gre";
+static const char* SETTINGS = "Ustawienia";
+static const char* SELECT_DIFFICULTY = "Wybierz poziom trudnosci";
+static const char* EXPERIENCED = "Doswiadczony student";
+static const char* EASY = "Poziom trudnosci: latwy";
+static const char* FRESHMAN = "Swiezak";
+static const char* HARD = "Poziom trudnosci: trudny";
+static const char* GO_BACK = "Wroc";
+
 Game::Game() : state(GameState::START), difficulty(GameDifficulty::EASY) {
     menuTexture = LoadTexture("assets/menu.png");
     settingsTexture = LoadTexture("assets/settings.png");
+    whiteMan = LoadTexture("assets/whiteMan.png");
+    redMan = LoadTexture("assets/redman.png");
 }
 
 Game::~Game() {
     UnloadTexture(menuTexture);
     UnloadTexture(settingsTexture);
+    UnloadTexture(whiteMan);
+    UnloadTexture(redMan);
 }
 
 void Game::Run() {
@@ -32,14 +47,36 @@ void Game::Run() {
 
 void Game::DrawStartMenu() {
     DrawBackground(menuTexture);
-    DrawText("Flanki", GetCenteredX("Flanki", 100), 120, 100, YELLOW);
-    if (TextButton("Nowa gra", {GetCenteredX("Nowa gra", 40), 250}, 40, WHITE, RED)) state = GameState::GAME;
-    if (TextButton("Wczytaj gre", {GetCenteredX("Wczytaj gre", 40), 320}, 40, WHITE, RED)) state = GameState::GAME;
-    if (TextButton("Ustawienia", {GetCenteredX("Ustawienia", 40), 390}, 40, WHITE, RED)) state = GameState::SETTINGS;
+
+    DrawText(GAME_NAME, GetCenteredX(GAME_NAME, 100), 120, 100, YELLOW);
+
+    if (TextButton(NEW_GAME, {GetCenteredX(NEW_GAME, 40), 250}, 40, WHITE, RED)) state = GameState::GAME;
+    if (TextButton(LOAD_GAME, {GetCenteredX(LOAD_GAME, 40), 320}, 40, WHITE, RED)) state = GameState::GAME;
+    if (TextButton(SETTINGS, {GetCenteredX(SETTINGS, 40), 390}, 40, WHITE, RED)) state = GameState::SETTINGS;
 }
 
 void Game::DrawSettingsMenu() {
     DrawBackground(settingsTexture);
+
+    DrawText(SELECT_DIFFICULTY, GetCenteredX(SELECT_DIFFICULTY, 45), 20, 45, YELLOW);
+
+    bool isEasyMode = (difficulty == GameDifficulty::EASY);
+    Color easyColor = isEasyMode ? RED : WHITE;
+    Color hardColor = isEasyMode ? WHITE : RED;
+    Texture2D easyTexture = isEasyMode ? redMan : whiteMan;;
+    Texture2D hardTexture = isEasyMode ? whiteMan : redMan;;
+
+    if (TextureButton(easyTexture, {100, 90}, WHITE)
+        or TextButton(EXPERIENCED, {GetCenteredXInBounds(EXPERIENCED, 20, 0, 400), 500}, 20, easyColor, easyColor)
+        or TextButton(EASY, {GetCenteredXInBounds(EASY, 20, 0, 400), 530}, 20, easyColor, easyColor))
+        difficulty = GameDifficulty::EASY;
+
+    if (TextureButton(hardTexture, {500, 90}, WHITE)
+        or TextButton(FRESHMAN, {GetCenteredXInBounds(FRESHMAN, 20, 400, 800), 500}, 20, hardColor, hardColor)
+        or TextButton(HARD, {GetCenteredXInBounds(HARD, 20, 400, 800), 530}, 20, hardColor, hardColor))
+        difficulty = GameDifficulty::HARD;
+
+    if (IsKeyPressed(KEY_SPACE)) state = GameState::START;
 }
 
 void Game::DrawGame() {
--- src/game.h
+++ src/game.h
@@ -15,6 +15,8 @@ private:
     GameDifficulty difficulty;
     Texture2D menuTexture;
     Texture2D settingsTexture;
+    Texture2D whiteMan;
+    Texture2D redMan;
     void DrawStartMenu();
     void DrawSettingsMenu();
     void DrawGame();
