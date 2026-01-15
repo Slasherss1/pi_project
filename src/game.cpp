18e73189110602e900935657eda9c1d567f3e481
Jan Szumotalski <jszumotalski@gmail.com>
2026-01-15 23:52:35 +0100

    added drink meter

--- src/game.cpp
+++ src/game.cpp
@@ -1,14 +1,17 @@
 #include "game.h"
+#include "drinkMeter.h"
 #include "masher.h"
 #include "utils.h"
 #include <raylib.h>
 #include <string>
 
 Masher mash = Masher();
+DrinkMeter drinkMeter = DrinkMeter();
 
 void Game::load() {
     bg = LoadTexture("assets/concrete.png");
     mash.text = "Pij!";
+    drinkMeter.sipSize = 0.1f;
 }
 
 void Game::unload() {
@@ -18,9 +21,14 @@ void Game::unload() {
 void Game::loop() {
     mash.tick();
 
+    if (mash.isPressed()) {
+        drinkMeter.sip();
+    }
+
     BeginDrawing();
     DrawBackground(bg);
     mash.draw();
+    drinkMeter.draw();
 
     EndDrawing();
 }
