555206128110a5ba5b2e41fa72856c6bc1d99833
Jan Szumotalski <jszumotalski@gmail.com>
2026-01-16 12:36:35 +0100

    Added outline and size macros

--- src/drinkMeter.cpp
+++ src/drinkMeter.cpp
@@ -1,6 +1,9 @@
 #include "drinkMeter.h"
 #include <raylib.h>
 
+#define DRINK_METER_WIDTH 64.0f
+#define DRINK_METER_HEIGHT 256.0f
+
 void DrinkMeter::sip() {
     currentLevel -= sipSize;
     if (currentLevel < 0.0f) {
@@ -14,11 +17,21 @@ void DrinkMeter::reset() {
 
 void DrinkMeter::draw() {
     DrawRectangleGradientV(
-        GetScreenWidth() - 32, 
-        GetScreenHeight()/2.0+128.0/2.0-128.0*currentLevel, 
-        32, 
-        128.0*currentLevel, 
+        GetScreenWidth() - DRINK_METER_WIDTH, 
+        GetScreenHeight()/2.0+DRINK_METER_HEIGHT/2.0-DRINK_METER_HEIGHT*currentLevel, 
+        DRINK_METER_WIDTH, 
+        DRINK_METER_HEIGHT*currentLevel, 
         {0xda,0x62,0x0a,0xff}, 
         {0xfe, 0xcd, 0x3b, 0x90}
+    );
+    DrawRectangleLinesEx(
+        {
+            GetScreenWidth() - DRINK_METER_WIDTH,
+            GetScreenHeight()/2.0f-DRINK_METER_HEIGHT/2,
+            DRINK_METER_WIDTH,
+            DRINK_METER_HEIGHT
+        },
+        5.0,
+        BLACK
     );
 }
